09e24fe0260f9172dcae43a09fc85e261cd833de
Marzena <marzena.szopinska090@gmail.com>
2019-03-14 18:18:01 +0100

    added pictures and created the page structure

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// <header>
+//   <h1>Marzena Szopinska</h1>
+//   <h3>Front-End Developer</h3>
+// </header>
+// <section class="main-work">
+//   <img src="images/main.jpeg" alt="image of an architecutre plan" class="main-picture">
+// </section>
+// <section class="featured-work">
+//   <h2>Featured Work</h2>
+//   <div class="image-container">
+//     <div class="image">
+//       <img src="images/1.jpeg" alt="image of an architecure">
+//       <p class="title">Title</p>
+//       <a href="#">link</a>
+//     </div>
+//     <div class="image">
+//       <img src="images/2.jpeg" alt="image of an architecure">
+//       <p class="title">Title</p>
+//       <a href="#">link</a>
+//     </div>
+//     <div class="image">
+//       <img src="images/3.jpeg" alt="image of an architecure">
+//       <p class="title">Title</p>
+//       <a href="#">link</a>
+//     </div>
+//   </div>
+// </section>
